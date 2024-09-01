4ef5451b64399da7c3b677efd2c41087afeac1ce
Kumar-VishalJai-swal <vishalkumarjais01@gmail.com>
2024-09-01 17:23:06 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./mainPage/navbar";
 import Hero from "./mainPage/Hero";
@@ -10,7 +9,7 @@ import About from "./mainPage/About"
 function App() {
  
   return (
-    <div className='bg-white text-black w-screen h-screen'>
+    <div>
       <Navbar />
       <Hero />
       <About />
